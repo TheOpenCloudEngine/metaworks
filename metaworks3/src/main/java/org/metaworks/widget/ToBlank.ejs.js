56ba42f513f620bae4c5f9a5f81f70d765775a2c
jinyoung jang <jyjang@uengine.org>
2016-04-20 09:49:38 +0900

    ToBlank implementation for opening url in a new tab

---
+++ metaworks3/src/main/java/org/metaworks/widget/ToBlank.ejs.js
@@ -0,0 +1,6 @@
+var org_metaworks_widget_ToBlank = function(objectId, className){
+
+    var object = mw3.objects[objectId];
+    window.open(object.url, "_blank");
+
+}
---
+++ metaworks3/src/main/java/org/metaworks/widget/ToBlank.java
@@ -0,0 +1,20 @@
+package org.metaworks.widget;
+
+/**
+ * Created by jangjinyoung on 16. 4. 19..
+ */
+public class ToBlank {
+
+    public ToBlank(String url){
+        setUrl(url);
+    }
+
+    String url;
+        public String getUrl() {
+            return url;
+        }
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+}
